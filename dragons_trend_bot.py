c158ea0ada1c3fc38b78855a1ecb5bf38961cf22
davemilk10-collab <davemilk10@gmail.com>
2025-09-25 01:13:29 +0100

    Update dragons_trend_bot.py

--- dragons_trend_bot.py
+++ dragons_trend_bot.py
@@ -5,7 +5,7 @@ CHANNEL_ID = "@dragonstrending"
 
 def vote(update: Update, context: CallbackContext):
     user_id = update.effective_user.id
-    chat_member = context.bot.get_chat_member(CHANNEL_ID, user_id)
+    chat_member = context.bot.get_chat_member(@dragonstrending, user_id)
 
     if chat_member.status in ["member", "administrator", "creator"]:
         project = " ".join(context.args)
@@ -15,7 +15,7 @@ def vote(update: Update, context: CallbackContext):
         else:
             update.message.reply_text("⚠️ Please specify a project name. Example: /vote Gashy")
     else:
-        update.message.reply_text("❌ You must join our channel first: https://t.me/YourChannel")
+        update.message.reply_text("❌ You must join our channel first: https://t.me/dragonstrending")
 
 updater = Updater("8323882698:AAG09u4nIL1ychjc1lk5H9bp8dEsKppA9UM")
 dp = updater.dispatcher
